20cc53fbf57ce8cd76eb550d7d5f78ec496bae4f
RashidSawalha <rashid.sawalhaxx@gmail.com>
2024-05-11 22:36:08 +0300

    Completed Task: 1-updateGuideList , 2-DeleteGuideFromList

--- models/Guide/guide_db_queries.js
+++ models/Guide/guide_db_queries.js
@@ -15,4 +15,53 @@ Guide.addGuide = (newGuide, result) => {
     });
   };
 
+  Guide.updateGuideList = (GuideID, Data, result) => {
+
+    const guideData = db.query(`SELECT * FROM guide Where GuideID = ${GuideID}`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        console.log(res);
+        let updateQuery = 'UPDATE guide SET ';
+        Object.keys(Data).forEach((key, index) => { // Object.keys(Data) is used for iterate key-values 
+            if (Data[key] !== '') {
+                updateQuery += `${key} = '${Data[key]}'`;
+                if (index < Object.keys(Data).length - 1) {
+                    updateQuery += ', ';
+                }
+            }
+        });
+        updateQuery += ` WHERE GuideID = ${GuideID}`;
+        console.log(updateQuery);
+        // Execute the update query
+        db.query(updateQuery, (err, result) => {
+            if (err) {
+                console.error('Error updating record:', err);
+                return;
+            }
+            console.log('Record updated successfully');
+        });
+
+        result(null, res);
+
+    });
+
+}
+
+
+Guide.deleteSpecificGuideFromList = (GuideID, result) => {
+  db.query(`DELETE FROM guide WHERE GuideID = "${GuideID}"`, (err, res) => {
+
+      if (err) {
+          console.log(3);
+          result(err, null);
+          return;
+      }
+      result(null, res);
+  });
+}
+
 module.exports = Guide;  
