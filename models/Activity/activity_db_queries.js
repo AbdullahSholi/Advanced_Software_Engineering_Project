bc80ea0db0948ac208fc20e8adf85fa19e1e8610
Abdullah Sholi <groupgroup060@gmail.com>
2024-06-06 18:25:11 +0300

    Completed Tasks: 1) CRUD Operations for Exchange, Resource and Planning Activity.

---
+++ models/Activity/activity_db_queries.js
@@ -0,0 +1,121 @@
+const db = require('../../db-connection');
+
+const Activity = {};
+
+// Add a new activity
+Activity.addActivity = (newActivity, result) => {
+    db.query('INSERT INTO `planning activity` SET ?', newActivity, (err, res) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+            return;
+        }
+        result(null, { ...newActivity });
+    });
+};
+
+// Get the list of all activities
+Activity.getActivitiesList = (result) => {
+    db.query('SELECT * FROM `planning activity`', (err, res) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+// Get the list of activities by ActivityID
+Activity.getActivitiesListById = (ActivityID, result) => {
+    db.query('SELECT * FROM `planning activity` WHERE ActivityID = ?', [ActivityID], (err, res) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+// Get the list of activities by UserID
+Activity.getActivitiesListByUserId = (UserID, result) => {
+    db.query('SELECT * FROM `planning activity` WHERE UserID = ?', [UserID], (err, res) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+// Get the list of activities by PlotID
+Activity.getActivitiesListByPlotId = (PlotID, result) => {
+    db.query('SELECT * FROM `planning activity` WHERE PlotID = ?', [PlotID], (err, res) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+
+
+// Update an activity
+Activity.updateActivity = (ActivityID, Data, result) => {
+    // Get the existing activity
+    db.query('SELECT * FROM `planning activity` WHERE ActivityID = ?', [ActivityID], (err, res) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+            return;
+        }
+        if (res.length === 0) {
+            result({ kind: "not_found" }, null);
+            return;
+        }
+        
+        // Build the update query
+        let updateQuery = 'UPDATE `planning activity` SET ';
+        let updateData = [];
+        Object.keys(Data).forEach((key, index) => {
+            if (Data[key] !== '') {
+                updateQuery += `${key} = ?`;
+                updateData.push(Data[key]);
+                if (index < Object.keys(Data).length - 1) {
+                    updateQuery += ', ';
+                }
+            }
+        });
+        updateQuery += ' WHERE ActivityID = ?';
+        updateData.push(ActivityID);
+        
+        // Execute the update query
+        db.query(updateQuery, updateData, (err, res) => {
+            if (err) {
+                console.error('Error updating record:', err);
+                result(err, null);
+                return;
+            }
+            console.log('Record updated successfully');
+            result(null, res);
+        });
+    });
+};
+
+// Delete an activity
+Activity.deleteActivity = (ActivityID, result) => {
+    db.query('DELETE FROM `planning activity` WHERE ActivityID = ?', [ActivityID], (err, res) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+module.exports = Activity;
---
+++ models/Exchange/exchange_db_queries.js
@@ -0,0 +1,143 @@
+const db = require('../../db-connection');
+
+const Exchange = {};
+
+Exchange.addExchange = (newExchange, result) => {
+
+    db.query('INSERT INTO exchange SET ?', newExchange, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, { ...newExchange });
+    });
+};
+
+Exchange.getExchangesList = (result) => {
+
+    db.query('SELECT * FROM exchange', (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+Exchange.getExchangesListById = (ExchangeID, result) => {
+
+    db.query(`SELECT * FROM exchange WHERE ExchangeID = ${ExchangeID}`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+Exchange.getExchangesListByResourceId = (ResourceID, result) => {
+    db.query(`SELECT * FROM exchange WHERE ResourceID = "${ResourceID}"`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+Exchange.getExchangesListByOfferUserId = (OfferUserID, result) => {
+    db.query(`SELECT * FROM exchange WHERE OfferUserID = "${OfferUserID}"`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+Exchange.getExchangesListByRequestorUserId = (RequestorUserID, result) => {
+    db.query(`SELECT * FROM exchange WHERE RequestorUserID = "${RequestorUserID}"`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+Exchange.getExchangesListByStatus = (Status, result) => {
+    db.query(`SELECT * FROM exchange WHERE Status = "${Status}"`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+///////////////
+
+
+Exchange.updateExchange = (ExchangeID, Data, result) => {
+
+    const ExchangeData = db.query(`SELECT * FROM exchange Where ExchangeID = ${ExchangeID}`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        console.log(res);
+        let updateQuery = 'UPDATE exchange SET ';
+        Object.keys(Data).forEach((key, index) => { // Object.keys(Data) is used for iterate key-values 
+            if (Data[key] !== '') {
+                updateQuery += `${key} = '${Data[key]}'`;
+                if (index < Object.keys(Data).length - 1) {
+                    updateQuery += ', ';
+                }
+            }
+        });
+        updateQuery += ` WHERE ExchangeID = ${ExchangeID}`;
+        console.log(updateQuery);
+        // Execute the update query
+        db.query(updateQuery, (err, result) => {
+            if (err) {
+                console.error('Error updating record:', err);
+                return;
+            }
+            console.log('Record updated successfully');
+        });
+
+        result(null, res);
+
+    });
+
+}
+
+
+Exchange.deleteExchange = (ExchangeID, result) => {
+    db.query(`DELETE FROM exchange WHERE ExchangeID = "${ExchangeID}"`, (err, res) => {
+
+        if (err) {
+            console.log(3);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+}
+module.exports = Exchange;  
