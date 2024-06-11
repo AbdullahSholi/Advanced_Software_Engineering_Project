ca42772a4384fd61cfdbd2c61a47b9019bcfb0e3
Abdullah Sholi <groupgroup060@gmail.com>
2024-06-11 21:38:00 +0300

    Completed Tasks: 1) Finish The Project

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,13 +3,15 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1];
+  console.log(token);
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
 
   try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const decoded = jwt.verify(token, process.env.SECRET_KEY, { expiresIn: '1h' });
     req.user = decoded;
     next();
   } catch (err) {
+    console.log(err);
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
